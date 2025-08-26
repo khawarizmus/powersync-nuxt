2b5319dc3f7d8f7f11f47eb95109da021ff6606a
khawarizmus <b.afatah@yahoo.com>
2025-08-26 12:53:00 +0800

    fix: fixed import statment

--- src/runtime/plugin.client.ts
+++ src/runtime/plugin.client.ts
@@ -1,4 +1,4 @@
-import { defineNuxtPlugin } from "#app";
+import { defineNuxtPlugin, useRuntimeConfig } from "#app";
 
 export default defineNuxtPlugin((nuxtApp) => {
   // Expose PowerSync module options globally
